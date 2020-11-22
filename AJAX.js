a7c6fcd9ed3752a712b636231aa26d5c4008751f
arkaprabhadey <euphoricdey420@gmail.com>
2020-11-22 14:27:33 +0530

    [Arka]Refactored:Made use of promise call and callback both

--- AJAX.js
+++ AJAX.js
@@ -1,11 +1,8 @@
 let XMLHttpRequest=require("xmlhttprequest").XMLHttpRequest;
-function showTime(){
-
-}
+//Using callback
 function makeAjaxCall(methodType,url,callback,async=true,data=null){
     let xhr= new XMLHttpRequest();
     xhr.onreadystatechange= function(){
-       // console.log(methodType+" State Change status"+xhr.status+"Ready state: "+xhr.readyState);
         if(xhr.readyState===4){
             if(xhr.status===200||xhr.status===201){
                 callback(xhr.responseText)
@@ -29,6 +26,38 @@ function makeAjaxCall(methodType,url,callback,async=true,data=null){
 
     console.log(methodType+" request sent to Server");
 }
+//Using promise
+function makePromiseCall(methodType,url,async=true,data=null){
+    return new Promise(function(resolve,reject){
+        let xhr= new XMLHttpRequest();
+        xhr.onreadystatechange= function(){
+            if(xhr.status.toString().match('^[2][0-9]{2}$')){
+                resolve(xhr.responseText);
+            }
+            else if(xhr.status.toString().match('^[4,5][0-9]{2}$')){
+                reject({
+                    status: xhr.status,
+                    statusText: xhr.statusText 
+                });
+                console.log("XHR Failed");
+            }
+        }
+        xhr.open(methodType,url,async);
+    if (data){
+        console.log(JSON.stringify(data));
+        xhr.setRequestHeader("Content-Type","application/json");
+        xhr.send(JSON.stringify(data));
+    }
+    else {
+        xhr.send();
+    }
+
+
+    console.log(methodType+" request sent to Server");
+
+
+    });
+}
 
 
 const getURL="http://localhost:3000/contacts/1";
@@ -36,12 +65,18 @@ function getUser(data){
     console.log("Get user data: "+data);
 }
 makeAjaxCall("GET",getURL,getUser,true);
+//Using Promise
+makePromiseCall("GET",getURL,true)
+    .then(response=>{
+        console.log("Get user data using promise:"+response)
+    })
+    .catch(error=>console.log("Get error status: "+JSON.stringify(error)));
 
-const deleteURL="http://localhost:3000/contacts/2";
+const deleteURL="http://localhost:3000/contacts/7";
 function getUserData(data){
     console.log("User Deleted: "+data);
 }
-makeAjaxCall("DELETE",deleteURL,getUserData,true);
+makeAjaxCall("DELETE",deleteURL,getUserData,false);
 
 const postURL="http://localhost:3000/contacts";
 const empDate= {
